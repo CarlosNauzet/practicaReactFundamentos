99c75450a27b7445f424a208610dd9941f0e3247
Carlos Nauzet Hernández <carlos.hernandez@motork.io>
2023-11-26 11:33:15 +0100

    Flujo para listar anuncios: redirecciona a Log in si no estás identificado

--- src/pages/adverts/AdvertsPage.jsx
+++ src/pages/adverts/AdvertsPage.jsx
@@ -1,7 +1,10 @@
 import { useEffect, useState } from "react";
 import { getAdverts } from "./service";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const AdvertsPage = () => {
+  const navigate = useNavigate();
   const [adverts, setAdverts] = useState([]);
 
   useEffect(() => {
@@ -11,12 +14,27 @@ const AdvertsPage = () => {
         setAdverts(adverts);
       } catch (error) {
         console.log(error);
+        toast.error("Unauthorized, please first proceed to log in");
+        navigate("/login");
       }
     };
     fetchAdverts();
   }, []);
 
-  return <div className="adverts">Aquí va el listado de anuncios</div>;
+  return (
+    <div className="adverts">
+      {adverts.map((advert, index) => {
+        return (
+          <div className="advert" key={index}>
+            <p>{advert.name}</p>
+            <p>{advert.price}</p>
+            <p>{advert.sale}</p>
+            <p>{advert.tags}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default AdvertsPage;
